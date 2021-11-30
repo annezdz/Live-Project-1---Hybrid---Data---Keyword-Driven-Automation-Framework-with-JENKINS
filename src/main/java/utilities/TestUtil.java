b2aefd01c71be9e2e50f8aaec5c37a081b48f27b
Anne Nicolle Zimmermann <58869569+annezdz@users.noreply.github.com>
2021-11-29 23:06:18 -0300

    creating common data provider and adding log4j how logger

--- src/main/java/utilities/TestUtil.java
+++ src/main/java/utilities/TestUtil.java
@@ -7,9 +7,11 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.DataProvider;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class TestUtil extends TestBase {
 
@@ -23,6 +25,22 @@ public class TestUtil extends TestBase {
         screenshotName = "error.png";
         FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") +
                 "\\target\\surefire-reports\\html\\" + screenshotName));
+    }
+
+    @DataProvider(name = "dp")
+    public static Object[][] getData(Method method) {
+        String sheetName = method.getName().toUpperCase();
+        int rows = excelReader.getRowCount(sheetName);
+        int cols = excelReader.getColumnCount(sheetName);
+
+        Object[][] data = new Object[rows - 1][cols];
+
+        for (int rowNum = 2; rowNum <= rows; rowNum++) {
+            for (int colNum = 0; colNum < cols; colNum++) {
+                data[rowNum - 2][colNum] = excelReader.getCellData(sheetName, colNum, rowNum);
+            }
+        }
+        return data;
     }
 
 
